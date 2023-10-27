55e1dd0b85356b6d22c7b9fac6983a8833cce62c
304k3 <304k3@WIN-C67EQSDJBQ1>
2023-10-27 20:51:02 +0300

    c

---
+++ UniversityServices/Pages/AddTeacher.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniversityServices.Pages
+{
+    public class AddTeacherModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

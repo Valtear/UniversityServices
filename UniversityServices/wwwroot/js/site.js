394067be2309d64b8ebdfb5c6425d1aeedff1655
Admin <Admin@WIN-C67EQSDJBQ1>
2023-11-01 20:51:37 +0300

    01.11

--- UniversityServices/wwwroot/js/site.js
+++ UniversityServices/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function redirectFunc(https://google.com) {
+    var btn = document.getElementById("btnRef")
+
+}
